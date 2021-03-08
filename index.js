b69c30b4e1fbdff04a8a2d6ad92c4c31f31e40e2
Don Maxito <maxharz@gmail.com>
2021-03-08 12:20:06 +0100

    Naming, comments

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import Post from './components/Post.js';
 const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 const data = await response.json();
 
-// Display result
-const blog = document.getElementById('blog');
-if (!blog) {
+// Render blog posts
+const $blog = document.getElementById('blog');
+if (!$blog) {
   throw new Error('Blog container not found');
 }
 
 for (const post of data) {
   const $post = new Post(post);
 
-  blog.appendChild($post);
+  $blog.appendChild($post);
 }
